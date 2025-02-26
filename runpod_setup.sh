f380260f8ddc37743f34e9758dfcdf55d5495738
“CooperBigFoot” <nlazaro@student.ethz.ch>
2025-02-25 19:44:44 -0500

    Add tensorboard package to installation script

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing all packages"
-pip install "numpy<=3.0.0" "torch>=2.0.0,!=2.0.1,<3.0.0" "lightning>=2.0.0,<3.0.0" "scipy>=1.8,<2.0" "pandas>=1.3.0,<3.0.0" "scikit-learn>=1.2,<2.0" "ipykernel>=6.29.5" "matplotlib>=3.10.0" "seaborn>=0.13.2" "pip-tools>=7.4.1" "cdsapi>=0.7.4" "geopandas>=1.0.1" "optuna>=4.2.1" "plotly>=6.0.0"
+pip install "numpy<=3.0.0" "torch>=2.0.0,!=2.0.1,<3.0.0" "lightning>=2.0.0,<3.0.0" "scipy>=1.8,<2.0" "pandas>=1.3.0,<3.0.0" "scikit-learn>=1.2,<2.0" "ipykernel>=6.29.5" "matplotlib>=3.10.0" "seaborn>=0.13.2" "pip-tools>=7.4.1" "cdsapi>=0.7.4" "geopandas>=1.0.1" "optuna>=4.2.1" "plotly>=6.0.0" "tensorboard>=2.19.0"
 
 # Configure Git user
 echo "Configuring Git..."
